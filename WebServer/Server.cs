6b26b4e628c3a85089f9beaca28abee0e38c8fd8
adrian <adrianvez11@gmail.com>
2025-07-24 01:39:26 -0400

    Web server can now run and it is viewable form the browser

--- WebServer/Server.cs
+++ WebServer/Server.cs
@@ -1,6 +1,8 @@
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace WebServer;
 
@@ -21,17 +23,69 @@ public static class Server
         return ret;
     }
 
+    public static void Start()
+    {
+        List<IPAddress> localhostIPs = GetLocalhostIPs();
+        HttpListener listener = InitializeListener(localhostIPs);
+        Start(listener);
+    }
+
     private static HttpListener InitializeListener(List<IPAddress> localhostIPs)
     {
         HttpListener listener = new HttpListener();
+        listener.Prefixes.Add("http://localhost:8080/");
 
-        //listen to the IP adress as well
+        //listen to the IP address as well
         localhostIPs.ForEach(ip =>
         {
-            Console.WriteLine("Listening on IP" + "http://" + ip.ToString() + "/");
-            listener.Prefixes.Add("http://" + ip.ToString() + "/");
+            Console.WriteLine("Listening on IP " + "http://" + ip.ToString() + ":8080/");
+            listener.Prefixes.Add("http://" + ip.ToString() + ":8080/");
         });
 
         return listener;
+    }
+
+    //Listens to connections on a seperate worker thread
+    private static void Start(HttpListener listener)
+    {
+        listener.Start();
+        Task.Run(() => RunServer(listener));
+    }
+
+    // Running in a separate thread, starts listening to connections not exceeding the max number of connections set
+    private static async Task RunServer(HttpListener listener)
+    {
+        while (true)
+        {
+            sem.WaitOne();
+            await StartConnectionListener(listener);
+        }
+    }
+
+    //log requests
+    public static void Log(HttpListenerRequest request)
+    {
+        Console.WriteLine(request.RemoteEndPoint + " " + request.HttpMethod + " /" + request.Url.AbsoluteUri);
+    }
+
+    //Asynchronously listen and wait for connectiosn
+    private static async Task StartConnectionListener(HttpListener listener)
+    {
+        //Wait for connections, return to caller while we wait
+        HttpListenerContext context = await listener.GetContextAsync();
+
+        //release the sempahore so that another listener can immediately start up
+        sem.Release();
+
+        //log the request
+        Log(context.Request);
+        
+
+
+        string response = "<html><head><meta http-equiv='content-type' content='text/html; charset=utf-8'/></head>Hello Browser!</html>";
+        byte[] encoded = Encoding.UTF8.GetBytes(response);
+        context.Response.ContentLength64 = encoded.Length;
+        context.Response.OutputStream.Write(encoded, 0, encoded.Length);
+        context.Response.OutputStream.Close();
     }
 }
